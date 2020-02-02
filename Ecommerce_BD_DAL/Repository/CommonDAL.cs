9fbf21e4999abfc0f348c8c0b10bc88e74ae94c4
Mohammad Sadikur Rahman <32734077+rsadikur66@users.noreply.github.com>
2020-02-02 14:54:53 +0600

    Add project files.

---
+++ Ecommerce_BD_DAL/Repository/CommonDAL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce_BD_DAL.Repository
+{
+    public class CommonDAL
+    {
+        private SqlTransaction sqlTransaction;
+        readonly SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString);
+
+        public void BeginTransaction()
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                sqlConnection.Open();
+                sqlTransaction = sqlConnection.BeginTransaction();
+            }
+        }
+
+        public void CommitTransaction()
+        {
+            sqlTransaction.Commit();
+        }
+        public void RollbackTransaction()
+        {
+            sqlTransaction.Rollback();
+        }
+
+
+        public DataTable Query(string query)
+        {
+            var sqlconnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlCon"].ConnectionString);
+            var sqlcommand = new SqlCommand
+            {
+                Connection = sqlconnection,
+                CommandText = query
+            };
+            var sqladapter = new SqlDataAdapter(sqlcommand);
+            var datatable = new DataTable();
+            sqladapter.Fill(datatable);
+            return datatable;
+        }
+
+        public bool Command(string command)
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+                sqlConnection.Open();
+            var sqlCommand = new SqlCommand
+            {
+                Connection = sqlConnection,
+                CommandText = command
+            };
+            return sqlCommand.ExecuteNonQuery() > 0;
+        }
+
+
+    }
+}
